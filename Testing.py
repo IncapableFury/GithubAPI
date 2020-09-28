a30e01c655c411438d26e697da32b5bad5968980
IncapableFury <41422991+IncapableFury@users.noreply.github.com>
2020-09-28 18:18:53 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -4,7 +4,7 @@ from start import *
 
 class TestGithubAPI(unittest.TestCase):
     def testRepoGetter(self):
-        assert get_repo_by_id("IncapableFury") == ["CS-546", "CS-554", "CS546_FinalProject-1", "Final_Project_SW555",
+        assert get_repo_names_by_id("IncapableFury") == ["CS-546", "CS-554", "CS546_FinalProject-1", "Final_Project_SW555",
                                                    "GithubAPI", "HomeWorks_For_EE552", "Triangle_Testing"]
 
     def testCommitGetter(self):
