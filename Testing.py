cc65405261f9b99d0e4044ac7d9aff83070876fb
IncapableFury <41422991+IncapableFury@users.noreply.github.com>
2020-09-28 18:15:28 -0400

    refine

---
+++ Testing.py
@@ -0,0 +1,17 @@
+import unittest
+from start import *
+
+
+class TestGithubAPI(unittest.TestCase):
+    def testRepoGetter(self):
+        assert get_repo_by_id("IncapableFury") == ["CS-546", "CS-554", "CS546_FinalProject-1", "Final_Project_SW555",
+                                                   "GithubAPI", "HomeWorks_For_EE552", "Triangle_Testing"]
+
+    def testCommitGetter(self):
+        assert get_commites("IncapableFury", "CS-546") == 30
+        assert get_commites("IncapableFury", "Triangle_Testing") == 10
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
--- start.py
+++ start.py
@@ -1,14 +1,21 @@
 import requests
 
+
+def get_repo_names_by_id(id):
+    return list(map(lambda x: x["name"], requests.get("https://api.github.com/users/{}/repos".format(id)).json()))
+
+
+def get_commites(id, repoName):
+    return len(requests.get("https://api.github.com/repos/{}/{}/commits".format(id, repoName)).json())
+
+
 if __name__ == "__main__":
-    # r = requests.get("https://api.github.com/users/IncapableFury/repos").json()
-    # for repo in r:
-    #     # repoName = repo["name"]
-    #     # c = requests.get("https://api.github.com/repos/IncapableFury/{}/commits".format(repoName)).json()
-    #     print('Repo: {:20} Number of commits: {}'.format(repo["name"], len(
-    #         requests.get("https://api.github.com/repos/IncapableFury/{}/commits".format(repo["name"])).json())))
+    repoNames = get_repo_names_by_id("IncapableFury")
+    for repoName in repoNames:
+        commits = get_commites("IncapableFury", repoName)
+        print('Repo: {:20} Number of commits: {}'.format(repoName, commits))
 
     # ---------fancy but nonsensical one-liner warning---------
-    print(*list(map(lambda repo: 'Repo: {:20} Number of commits: {}'.format(repo["name"], len(
-        requests.get("https://api.github.com/repos/IncapableFury/{}/commits".format(repo["name"])).json())),
-                        requests.get("https://api.github.com/users/IncapableFury/repos").json())), sep='\n')
+    # print(*list(map(lambda repo: 'Repo: {:20} Number of commits: {}'.format(repo["name"], len(
+    #     requests.get("https://api.github.com/repos/IncapableFury/{}/commits".format(repo["name"])).json())),
+    #                 requests.get("https://api.github.com/users/IncapableFury/repos").json())), sep='\n')
